ad3cb23ff1093abf405880762327ceff86a6fe05
ebanchik <ebanchik@gmail.com>
2024-03-21 15:11:06 -0400

    tweak rows, add bacgrkound to name

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -2,7 +2,7 @@
 
 export function Banner() {
   return (
-  <div className="container text-center">
+  <div className="container-fluid text-center">
       <div className="col">
         <h1 className="name">DIMA</h1>
       </div>
@@ -46,7 +46,6 @@ export function Banner() {
     </div>
   </div>
 
-  
   <div className="row">
     <div className="col-2">
       <div className="frame-container">
@@ -127,6 +126,7 @@ export function Banner() {
   </div>
 
 
+
   <div className="row">
     <div className="col-2">
       <div className="frame-container">
@@ -165,6 +165,89 @@ export function Banner() {
       </div>
     </div>
   </div>
+
+
+
+  <div className="row">
+    <div className="col-2">
+      <div className="frame-container">
+        <img src="assets/frame1.png" alt="Frame 1" className="frame" />
+        <img src="assets/chair.png" alt="Image 1" className="inner-image" />
+      </div>
+    </div>
+    <div className="col-2">
+      <div className="frame-container">
+        <img src="assets/frame1.png" alt="Frame 2" className="frame" />
+        <img src="assets/chair.png" alt="Image 2" className="inner-image" />
+      </div>
+    </div>
+    <div className="col-2">
+      <div className="frame-container">
+        <img src="assets/frame1.png" alt="Frame 2" className="frame" />
+        <img src="assets/chair.png" alt="Image 2" className="inner-image" />
+      </div>
+    </div>
+    <div className="col-2">
+      <div className="frame-container">
+        <img src="assets/frame1.png" alt="Frame 2" className="frame" />
+        <img src="assets/chair.png" alt="Image 2" className="inner-image" />
+      </div>
+    </div>
+    <div className="col-2">
+      <div className="frame-container">
+        <img src="assets/frame1.png" alt="Frame 2" className="frame" />
+        <img src="assets/chair.png" alt="Image 2" className="inner-image" />
+      </div>
+    </div>
+    <div className="col-2">
+      <div className="frame-container">
+        <img src="assets/frame1.png" alt="Frame 2" className="frame" />
+        <img src="assets/chair.png" alt="Image 2" className="inner-image" />
+      </div>
+    </div>
+  </div>
+
+
+
+  <div className="row">
+    <div className="col-2">
+      <div className="frame-container">
+        <img src="assets/frame1.png" alt="Frame 1" className="frame" />
+        <img src="assets/chair.png" alt="Image 1" className="inner-image" />
+      </div>
+    </div>
+    <div className="col-2">
+      <div className="frame-container">
+        <img src="assets/frame1.png" alt="Frame 2" className="frame" />
+        <img src="assets/chair.png" alt="Image 2" className="inner-image" />
+      </div>
+    </div>
+    <div className="col-2">
+      <div className="frame-container">
+        <img src="assets/frame1.png" alt="Frame 2" className="frame" />
+        <img src="assets/chair.png" alt="Image 2" className="inner-image" />
+      </div>
+    </div>
+    <div className="col-2">
+      <div className="frame-container">
+        <img src="assets/frame1.png" alt="Frame 2" className="frame" />
+        <img src="assets/chair.png" alt="Image 2" className="inner-image" />
+      </div>
+    </div>
+    <div className="col-2">
+      <div className="frame-container">
+        <img src="assets/frame1.png" alt="Frame 2" className="frame" />
+        <img src="assets/chair.png" alt="Image 2" className="inner-image" />
+      </div>
+    </div>
+    <div className="col-2">
+      <div className="frame-container">
+        <img src="assets/frame1.png" alt="Frame 2" className="frame" />
+        <img src="assets/chair.png" alt="Image 2" className="inner-image" />
+      </div>
+    </div>
+  </div>
+
 
 </div>
   )
