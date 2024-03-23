8cef6297db9a8aeab85a459e2cf707715ede976f
ebanchik <ebanchik@gmail.com>
2024-03-23 14:28:24 -0400

    add new images

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -10,38 +10,32 @@ export function Banner() {
   <div className="row">
     <div className="col-xl-2 col-l-2 col-md-3 col-sm-4 col-6 item">
       <div className="frame-container">
-        <img src="assets/frame1.png" alt="Frame 1" className="frame" />
-        <img src="assets/chair.png" alt="Image 1" className="inner-image" />
+        <img src="assets/frame1.png" alt="Image 1" className="frame" />
       </div>
     </div>
     <div className="col-xl-2 col-l-2 col-md-3 col-sm-4 col-6 item">
       <div className="frame-container">
-        <img src="assets/frame1.png" alt="Frame 2" className="frame" />
-        <img src="assets/chair.png" alt="Image 2" className="inner-image" />
+        <img src="assets/frame2.png" alt="Image 2" className="frame" />
       </div>
     </div>
     <div className="col-xl-2 col-l-2 col-md-3 col-sm-4 col-6 item">
       <div className="frame-container">
-        <img src="assets/frame1.png" alt="Frame 2" className="frame" />
-        <img src="assets/chair.png" alt="Image 2" className="inner-image" />
+        <img src="assets/frame3.png" alt="Image 2" className="frame" />
       </div>
     </div>
     <div className="col-xl-2 col-l-2 col-md-3 col-sm-4 col-6 item">
       <div className="frame-container">
-        <img src="assets/frame5.png" alt="Frame 2" className="frame" />
-        <img src="assets/chair.png" alt="Image 2" className="inner-image" />
+        <img src="assets/frame4.png" alt="Image 2" className="frame" />
       </div>
     </div>
     <div className="col-xl-2 col-l-2 col-md-3 col-sm-4 col-6 item">
       <div className="frame-container">
-        <img src="assets/frame1.png" alt="Frame 2" className="frame" />
-        <img src="assets/chair.png" alt="Image 2" className="inner-image" />
+        <img src="assets/frame5.png" alt="Image 2" className="frame" />
       </div>
     </div>
     <div className="col-xl-2 col-l-2 col-md-3 col-sm-4 col-6 item">
       <div className="frame-container">
-        <img src="assets/frame1.png" alt="Frame 2" className="frame" />
-        <img src="assets/chair.png" alt="Image 2" className="inner-image" />
+        <img src="assets/frame6.png" alt="Image 2" className="frame" />
       </div>
     </div>
   </div>
@@ -49,38 +43,32 @@ export function Banner() {
   <div className="row">
     <div className="col-xl-2 col-l-2 col-md-3 col-sm-4 col-6 item">
       <div className="frame-container">
-        <img src="assets/frame1.png" alt="Frame 1" className="frame" />
-        <img src="assets/chair.png" alt="Image 1" className="inner-image" />
+        <img src="assets/frame7.png" alt="Image 1" className="frame" />
       </div>
     </div>
     <div className="col-xl-2 col-l-2 col-md-3 col-sm-4 col-6 item">
       <div className="frame-container">
-        <img src="assets/frame5.png" alt="Frame 2" className="frame" />
-        <img src="assets/chair.png" alt="Image 2" className="inner-image" />
+        <img src="assets/frame8.png" alt="Image 2" className="frame" />
       </div>
     </div>
     <div className="col-xl-2 col-l-2 col-md-3 col-sm-4 col-6 item">
       <div className="frame-container">
-        <img src="assets/frame1.png" alt="Frame 2" className="frame" />
-        <img src="assets/chair.png" alt="Image 2" className="inner-image" />
+        <img src="assets/frame9.png" alt="Image 2" className="frame" />
       </div>
     </div>
     <div className="col-xl-2 col-l-2 col-md-3 col-sm-4 col-6 item">
       <div className="frame-container">
-        <img src="assets/frame1.png" alt="Frame 2" className="frame" />
-        <img src="assets/chair.png" alt="Image 2" className="inner-image" />
+        <img src="assets/frame10.png" alt="Image 2" className="frame" />
       </div>
     </div>
     <div className="col-xl-2 col-l-2 col-md-3 col-sm-4 col-6 item">
       <div className="frame-container">
-        <img src="assets/frame5.png" alt="Frame 2" className="frame" />
-        <img src="assets/chair.png" alt="Image 2" className="inner-image" />
+        <img src="assets/frame11.png" alt="Image 2" className="frame" />
       </div>
     </div>
     <div className="col-xl-2 col-l-2 col-md-3 col-sm-4 col-6 item">
       <div className="frame-container">
-        <img src="assets/frame1.png" alt="Frame 2" className="frame" />
-        <img src="assets/chair.png" alt="Image 2" className="inner-image" />
+        <img src="assets/frame12.png" alt="Image 2" className="frame" />
       </div>
     </div>
   </div>
@@ -89,82 +77,70 @@ export function Banner() {
   <div className="row">
     <div className="col-xl-2 col-l-2 col-md-3 col-sm-4 col-6 item">
       <div className="frame-container">
-        <img src="assets/frame5.png" alt="Frame 1" className="frame" />
-        <img src="assets/chair.png" alt="Image 1" className="inner-image" />
+        <img src="assets/frame13.png" alt="Image 1" className="frame" />
       </div>
     </div>
     <div className="col-xl-2 col-l-2 col-md-3 col-sm-4 col-6 item">
       <div className="frame-container">
-        <img src="assets/frame1.png" alt="Frame 2" className="frame" />
-        <img src="assets/chair.png" alt="Image 2" className="inner-image" />
+        <img src="assets/frame14.png" alt="Image 2" className="frame" />
       </div>
     </div>
     <div className="col-xl-2 col-l-2 col-md-3 col-sm-4 col-6 item">
       <div className="frame-container">
-        <img src="assets/frame1.png" alt="Frame 2" className="frame" />
-        <img src="assets/chair.png" alt="Image 2" className="inner-image" />
+        <img src="assets/frame15.png" alt="Image 2" className="frame" />
       </div>
     </div>
     <div className="col-xl-2 col-l-2 col-md-3 col-sm-4 col-6 item">
       <div className="frame-container">
-        <img src="assets/frame1.png" alt="Frame 2" className="frame" />
-        <img src="assets/chair.png" alt="Image 2" className="inner-image" />
+        <img src="assets/frame16.png" alt="Image 2" className="frame" />
       </div>
     </div>
     <div className="col-xl-2 col-l-2 col-md-3 col-sm-4 col-6 item">
       <div className="frame-container">
-        <img src="assets/frame1.png" alt="Frame 2" className="frame" />
-        <img src="assets/chair.png" alt="Image 2" className="inner-image" />
+        <img src="assets/frame17.png" alt="Image 2" className="frame" />
       </div>
     </div>
     <div className="col-xl-2 col-l-2 col-md-3 col-sm-4 col-6 item">
       <div className="frame-container">
-        <img src="assets/frame1.png" alt="Frame 2" className="frame" />
-        <img src="assets/chair.png" alt="Image 2" className="inner-image" />
+        <img src="assets/frame18.png" alt="Image 2" className="frame" />
       </div>
     </div>
   </div>
 
 
 
-  <div className="row">
+  {/* <div className="row">
     <div className="col-xl-2 col-l-2 col-md-3 col-sm-4 col-6 item">
       <div className="frame-container">
-        <img src="assets/frame1.png" alt="Frame 1" className="frame" />
         <img src="assets/chair.png" alt="Image 1" className="inner-image" />
       </div>
     </div>
     <div className="col-xl-2 col-l-2 col-md-3 col-sm-4 col-6 item">
       <div className="frame-container">
-        <img src="assets/frame1.png" alt="Frame 2" className="frame" />
         <img src="assets/chair.png" alt="Image 2" className="inner-image" />
       </div>
     </div>
     <div className="col-xl-2 col-l-2 col-md-3 col-sm-4 col-6 item">
       <div className="frame-container">
-        <img src="assets/frame5.png" alt="Frame 2" className="frame" />
         <img src="assets/chair.png" alt="Image 2" className="inner-image" />
       </div>
     </div>
     <div className="col-xl-2 col-l-2 col-md-3 col-sm-4 col-6 item">
       <div className="frame-container">
-        <img src="assets/frame1.png" alt="Frame 2" className="frame" />
         <img src="assets/chair.png" alt="Image 2" className="inner-image" />
       </div>
     </div>
     <div className="col-xl-2 col-l-2 col-md-3 col-sm-4 col-6 item">
       <div className="frame-container">
-        <img src="assets/frame5.png" alt="Frame 2" className="frame" />
         <img src="assets/chair.png" alt="Image 2" className="inner-image" />
       </div>
     </div>
     <div className="col-xl-2 col-l-2 col-md-3 col-sm-4 col-6 item">
       <div className="frame-container">
-        <img src="assets/frame1.png" alt="Frame 2" className="frame" />
         <img src="assets/chair.png" alt="Image 2" className="inner-image" />
       </div>
     </div>
-  </div>
+  </div> */}
 
 </div>
   )
