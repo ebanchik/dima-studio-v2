af0da1b7e16bc7bdff74a54f33dc5f003c372146
ebanchik <ebanchik@gmail.com>
2024-03-27 15:59:50 -0400

    new design idea

--- src/About.jsx
+++ src/About.jsx
@@ -1,10 +1,15 @@
+import { Link } from 'react-router-dom';  
+
 export function About() {
   return (
     <div className="page about-page">
       <div className="container">
+      <h1 className="button">
+          <Link to="/">DIMA</Link>
+        </h1>
+            <h1 className="about-title">THE CREATOR</h1>
         <div className="row">
           <div className="col-md-8">
-            <h1 className="about-title">ABOUT</h1>
             <p className="about-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga voluptas sed velit, dicta modi, rem harum architecto mollitia est accusamus deserunt! Quisquam doloribus libero reiciendis vero eius culpa praesentium ipsa. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam amet quod eligendi quo. Iusto perspiciatis maiores corporis fugit, expedita vitae excepturi, unde libero quod praesentium, deleniti qui atque tempora asperiores.
             Doloremque temporibus sed, magni, commodi beatae cupiditate eaque sunt nulla fuga dolorum consequatur voluptatum voluptates. Laudantium, illum. Velit debitis dolore maxime est accusantium, rem ab quasi neque, voluptates, sequi nihil.
             Debitis asperiores nisi impedit aspernatur distinctio nulla veritatis dolorum sed labore atque. In rerum temporibus officiis voluptatum quia obcaecati nisi, delectus quam quisquam saepe totam ipsum eligendi laudantium esse nulla?
