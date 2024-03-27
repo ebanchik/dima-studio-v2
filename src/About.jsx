6d868711fe5d55df104f95a16302386f8cdc43e9
ebanchik <ebanchik@gmail.com>
2024-03-27 14:53:12 -0400

    organize about page and add photo

--- src/About.jsx
+++ src/About.jsx
@@ -1,12 +1,20 @@
-
 export function About() {
   return (
-      <div className="page about-page">
-        <h1>About</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga voluptas sed velit, dicta modi, rem harum architecto mollitia est accusamus deserunt! Quisquam doloribus libero reiciendis vero eius culpa praesentium ipsa. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam amet quod eligendi quo. Iusto perspiciatis maiores corporis fugit, expedita vitae excepturi, unde libero quod praesentium, deleniti qui atque tempora asperiores.
-        Doloremque temporibus sed, magni, commodi beatae cupiditate eaque sunt nulla fuga dolorum consequatur voluptatum voluptates. Laudantium, illum. Velit debitis dolore maxime est accusantium, rem ab quasi neque, voluptates, sequi nihil.
-        Debitis asperiores nisi impedit aspernatur distinctio nulla veritatis dolorum sed labore atque. In rerum temporibus officiis voluptatum quia obcaecati nisi, delectus quam quisquam saepe totam ipsum eligendi laudantium esse nulla?
-        Odit laudantium inventore sequi expedita officiis ullam autem. Pariatur soluta culpa asperiores dolorum magnam ex, distinctio facere voluptatem numquam et atque eligendi voluptatibus ratione excepturi? Nulla enim quae natus numquam.</p>
+    <div className="page about-page">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8">
+            <h1 className="about-title">ABOUT</h1>
+            <p className="about-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga voluptas sed velit, dicta modi, rem harum architecto mollitia est accusamus deserunt! Quisquam doloribus libero reiciendis vero eius culpa praesentium ipsa. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam amet quod eligendi quo. Iusto perspiciatis maiores corporis fugit, expedita vitae excepturi, unde libero quod praesentium, deleniti qui atque tempora asperiores.
+            Doloremque temporibus sed, magni, commodi beatae cupiditate eaque sunt nulla fuga dolorum consequatur voluptatum voluptates. Laudantium, illum. Velit debitis dolore maxime est accusantium, rem ab quasi neque, voluptates, sequi nihil.
+            Debitis asperiores nisi impedit aspernatur distinctio nulla veritatis dolorum sed labore atque. In rerum temporibus officiis voluptatum quia obcaecati nisi, delectus quam quisquam saepe totam ipsum eligendi laudantium esse nulla?
+            Odit laudantium inventore sequi expedita officiis ullam autem. Pariatur soluta culpa asperiores dolorum magnam ex, distinctio facere voluptatem numquam et atque eligendi voluptatibus ratione excepturi? Nulla enim quae natus numquam.</p>
+          </div>
+          <div className="col-md-4">
+            <img src="assets/anika-pfp.jpeg" alt="Description" className="about-photo" />
+          </div>
+        </div>
       </div>
-  )
+    </div>
+  );
 }
