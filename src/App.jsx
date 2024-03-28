493c3709c409b56e8b4516455cfda0b3b2f7565b
ebanchik <ebanchik@gmail.com>
2024-03-28 13:57:18 -0400

    feed page and embed insta feed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 // import { Header } from './Header'
 import { Content } from './Content'
 import { About } from './About'
+import { Feed } from './Feed'
 import "./App.css"
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Content />} />
           <Route path="/about" element={<About />} />
+          <Route path="/feed" element={<Feed />} />
         </Routes>
       </div>
     </Router>
---
+++ src/Feed.jsx
@@ -0,0 +1,17 @@
+export function Feed() {
+  return (
+    <div className="feed">
+      <script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script>
+      <iframe
+        src="//lightwidget.com/widgets/b6d4455b732e5762a43a69588bc90583.html"
+        className="lightwidget-widget"
+        style={{
+          width: '100%',
+          height: '100%',
+          border: 0,
+          overflow: 'hidden'
+        }}
+      ></iframe>
+    </div>
+  );
+}
