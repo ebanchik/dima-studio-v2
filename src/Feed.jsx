f2130a9939e1fc86e1b34fa8e8c175645fe55595
ebanchik <ebanchik@gmail.com>
2024-03-28 14:24:48 -0400

    add header button, fix feed

--- src/Feed.jsx
+++ src/Feed.jsx
@@ -1,6 +1,21 @@
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
 export function Feed() {
+  const [isExiting, setIsExiting] = useState(false);
+  const navigate = useNavigate();
+
+  const handleExit = () => {
+    setIsExiting(true);
+    setTimeout(() => navigate('/'), 500); // Match the duration of the slideDown animation
+  };
+
   return (
-    <div className="feed">
+    <div>
+      <h1 className="button feed-button">
+        <Link to="/" onClick={handleExit}>DIMA</Link>
+      </h1>
+    <div className={`page feed-page ${isExiting ? 'feed-page-exit' : 'feed-page-enter'}`}>
       <script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script>
       <iframe
         src="//lightwidget.com/widgets/b6d4455b732e5762a43a69588bc90583.html"
@@ -12,6 +27,7 @@ export function Feed() {
           overflow: 'hidden'
         }}
       ></iframe>
+    </div>
     </div>
   );
 }
