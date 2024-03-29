a3da5fdf9a43fafd104477bc26abd7683de229af
ebanchik <ebanchik@gmail.com>
2024-03-29 16:08:31 -0400

    custom cursor

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,24 @@ import './App.css'
 import { Content } from './Content'
 import { About } from './About'
 import { Feed } from './Feed'
+import { CustomCursor } from "./CustomCursor";
 import "./App.css"
 
 function App() {
   
   return (
-    <Router>
-      <div className="app-container">
-        <Routes>
-          <Route path="/" element={<Content />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/feed" element={<Feed />} />
-        </Routes>
-      </div>
-    </Router>
+    <div>
+      <Router>
+      < CustomCursor />
+        <div className="app-container">
+          <Routes>
+            <Route path="/" element={<Content />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/feed" element={<Feed />} />
+          </Routes>
+        </div>
+      </Router>
+    </div>
   )
 }
 
