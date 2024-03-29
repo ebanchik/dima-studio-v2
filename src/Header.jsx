6eb8c5c88eff57e4bf66990df82482e1cd2286a5
ebanchik <ebanchik@gmail.com>
2024-03-29 15:48:04 -0400

    header hover shrink

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,7 +2,7 @@ export function Header() {
   return (
     <header>
       <nav className="header">
-        <a href="/">HOME</a> <a href="/about">ABOUT</a> <a href="/feed">FEED</a>
+        <a className="link" href="/">HOME</a> <a className="link" href="/about">ABOUT</a> <a className="link" href="/feed">FEED</a>
       </nav>
     </header>
   )
