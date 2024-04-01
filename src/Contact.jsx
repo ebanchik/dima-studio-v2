f4b467a0c6b516690fa1f5e30f43988b5e7c2556
ebanchik <ebanchik@gmail.com>
2024-04-01 15:38:57 -0400

    contact page slide up animation

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -2,7 +2,7 @@ import { Header } from './Header'
 
 export function Contact() {
   return (
-    <div>
+    <div className='page'>
       <Header />
       <div className="inquire-container">
         <a href="mailto:example@example.com" className="inquire">Inquire</a>
