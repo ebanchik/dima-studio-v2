d817c8082e531376082d3c99ebdfd5b3202aa784
ebanchik <ebanchik@gmail.com>
2024-04-01 14:24:14 -0400

    add inquire link

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,11 +1,13 @@
 import { Header } from './Header'
 
 export function Contact() {
-
   return (
     <div>
       <Header />
-      <p className="inquire">Inquire</p>
+      <p className="inquire">
+        <a className="inquire" href="mailto:your-email@example.com">Inquire</a>
+      </p>
     </div>
-  )
+  );
 }
+
