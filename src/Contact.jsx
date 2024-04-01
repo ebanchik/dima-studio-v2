20b3a386b8f7e602dbaeb17e68893057165d3691
ebanchik <ebanchik@gmail.com>
2024-04-01 16:19:22 -0400

    add button on contact page and fix inquire placement

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,9 +1,17 @@
+import { useNavigate } from 'react-router-dom';
 import { Header } from './Header'
 
 export function Contact() {
+  const navigate = useNavigate();
+
+  const handleExit = () => {
+    navigate('/');
+  };
+
   return (
     <div className='page'>
       <Header />
+      <button className="button" onClick={handleExit}>DIMA</button>
       <div className="inquire-container">
         <a href="mailto:example@example.com" className="inquire">Inquire</a>
       </div>
