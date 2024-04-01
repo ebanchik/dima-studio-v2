0596ce3b72140e0e95458c678e3759d858b33291
ebanchik <ebanchik@gmail.com>
2024-04-01 14:50:05 -0400

    add contact to header

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,6 +3,7 @@ export function Header() {
     <header>
       <nav className="header">
         <a className="link" href="/">HOME</a> <a className="link" href="/about">ABOUT</a> <a className="link" href="/feed">FEED</a>
+        <a className="link" href="/contact">CONTACT</a>
       </nav>
     </header>
   )
